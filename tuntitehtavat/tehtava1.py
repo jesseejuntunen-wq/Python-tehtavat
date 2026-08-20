50b43b353f9c2a65a77f9c6c015f932baf47094b
jesseejuntunen-wq <jesse.e.juntunen@gmail.com>
2026-08-20 10:15:58 +0300

    tehtava tehty

---
+++ tuntitehtavat/tehtava1.py
@@ -0,0 +1,4 @@
+nimi = input("anna  nimi")
+bday = input("anna syntymävuotesi")
+silma = input("sano joku  silmien väri")
+print(f"Olipa kerran ihmeellinen ihminen jonka nimi sattui olemaan {nimi}.Meidän eriskummallinen nimisen ihmisen tarina alkoi vuodesta {bday} mutta se ei ole tärkeää.Anyways siellä oli myös lohikäärme jonka silmien väri oli{silma} ja {nimi} valitettavasti kuoli sille   ")
